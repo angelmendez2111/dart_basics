8466d16db33dec682475ac362bf9cff7a0ce5eb0
angelmendez2111 <angelpieromendezquinones@gmail.com>
2025-10-21 17:58:12 -0500

    first change

---
+++ bin/Main.dart
@@ -0,0 +1,5 @@
+void main() {
+  int a = 3;
+  print(a);
+  print('esta es la priemra prueba en dart');
+}
--- bin/dart_basics.dart
+++ bin/dart_basics.dart
@@ -1,3 +1,27 @@
 void main() {
-  print('Hola mundo desde Dart!');
+  int acumulated = 0;
+  int incremento = 10;
+  bool bandera = false;
+  var mensaje = "El acumulado es: ";
+
+
+
+  while(bandera == false){
+    acumulated += incremento;
+
+    if(acumulated >= 50){
+      bandera = true;
+    }
+  }
+  print(mensaje + acumulated.toString());
+
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+  for (final object in flybyObjects) {
+    print(object);
+  }
+
+  final variableFinal = 4;
+  //variableFinal = 3;
+
 }
